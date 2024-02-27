53c519023bd210a2b8b4d6cb40f39329c7dac3c1
SamoelDuarte <129750291+SamoelDuarte@users.noreply.github.com>
2024-02-27 17:17:08 -0300

    s

--- resources/js/app.js
+++
@@ -1,9 +0,0 @@
-import './bootstrap';
-
-
-
-window.Echo.channel('new-orders')
-    .listen('.NewOrderEvent', (e) => {
-        // Aqui você pode exibir a notificação no ícone de sino na barra de navegação
-        // e informar o usuário sobre o novo pedido.
-    });
--- resources/views/front/home/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('front/layouts/main')
-
-@section('content')
-    
-@endsection
