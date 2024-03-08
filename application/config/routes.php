44264e19e3e0d6316c5d9d644b27d717f7c44dc8
SamoelDuarte <129750291+SamoelDuarte@users.noreply.github.com>
2024-03-08 18:33:09 -0300

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -55,7 +55,7 @@ $route['reports/(specific_:any)/(:any)/(:any)/(:any)'] = "reports/$1/$2/$3/$4";
 $route['reports/specific_customer'] = "reports/specific_customer_input";
 $route['reports/specific_employee'] = "reports/specific_employee_input";
 $route['device/getStatus'] = "device/getStatus";
-$route['device/updateStatus'] = 'Device/updateStatus'; // Certifique-se de que a rota esteja configurada corretamente
+$route['device/updateStatus'] = 'device/updateStatus'; // Certifique-se de que a rota esteja configurada corretamente
 $route['scaffolding_trigger'] = "";
 
 $route['404_override'] = 'errors/page_missing';
